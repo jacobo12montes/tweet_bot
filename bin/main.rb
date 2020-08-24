67db828aaae0bcbc24390f2bda84b0a1a782e258
Juan <jacobo12.montes@gmail.com>
2020-08-24 16:14:37 -0500

    FIx the exit od the loop

--- bin/main.rb
+++ bin/main.rb
@@ -7,38 +7,38 @@ loop do
   puts "\n"
   puts "\n"
   puts "                            WELLCOME TO FINDING BOT \n
-  It will help you to find tweets base on your key word\n 
+  |||||||||It will help you to find tweets base on your key word|||||||||\n 
   To start using it type your key word/words or the user name of your choice" 
   user = bot.searcher(gets.chomp)
   user.each { |key, value| puts "USER ID = #{key} \n\n LOCATION = #{value[0]} \n\n TWEET = #{value[4]} \n\n" }
   user.each { |key, value| p "Posible bots \n\n #{key} = #{value[5]}" if bot.check(value[2], value[3]) }
   puts "Would you like to check the information of a user?? (y|n)"
   resp = gets.chomp
-  if resp.eql?('y')
-    puts "Enter the user id"
+  if resp.eql?('y') || resp.eql?('Y')
+    puts 'Enter the user id'
     resp = gets.chomp
     user.each do |key, value|
       if key.eql?(resp)
-        puts "\n User information"
-        puts "\nID  => #{key}"
-        puts "Name => #{value[1]}"
-        puts "tweet(s)  => #{value[5]}"
-        puts "following => #{value[2]}"
-        puts "followers => #{value[3]}"
-        puts "=>Full text"
+        puts "\n User information
+        \nID  => #{key}
+Name => #{value[1]}
+tweet(s)  => #{value[5]}
+following => #{value[2]}
+followers => #{value[3]}"
+        puts "=>Tweeters text"
         for t in value[4]
-          puts "\n=> #{t}"
-        end
-        puts 'Would like to exit? (y|n)'
-        resp = gets.chomp
-        break if resp.eql?('y')  
+          puts "\n =>Tweeters text => #{t}"
+        end  
       end
     end
-  else
-    puts 'Would like to exit? (y|n)'
-    resp = gets.chomp
-    break if resp.eql?('y')
-  end 
+  # else
+  #   puts 'Would like to exit? (y|n)'
+  #   resp = gets.chomp
+  #   break if resp.eql?('y') || resp.eql?('Y')
+  end
+  puts 'Would like to exit? (y|n)'
+        resp = gets.chomp
+        break if resp.eql?('y') || resp.eql?('Y')
 end
 
 
