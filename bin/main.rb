e294597058111ac4e0d5a490fb2414a314f2e5dc
Juan <jacobo12.montes@gmail.com>
2020-08-24 16:33:27 -0500

    Fix the linter offenses

--- bin/main.rb
+++ bin/main.rb
@@ -10,7 +10,7 @@ loop do
   To start using it type your key word/words or the user name of your choice"
   user = bot.searcher(gets.chomp)
   user.each { |key, value| puts "USER ID = #{key} \n\n LOCATION = #{value[0]} \n\n TWEET = #{value[4]} \n\n" }
-  user.each { |key, value| p "Posible bots \n\n #{key} = #{value[5]}" if bot.check(value[2], value[3]) }
+  user.each { |key, value| p "\n Posible bot #{key} = #{value[5]}" if bot.check(value[2], value[3]) }
   puts 'Would you like to check the information of a user?? (y|n)'
   resp = gets.chomp
   if resp.eql?('y') || resp.eql?('Y')
@@ -18,13 +18,13 @@ loop do
     resp = gets.chomp
     user.each do |key, value|
       next unless key.eql?(resp)
+
       puts "\n User information
               \nID  => #{key}
       Name => #{value[1]}
       tweet(s)  => #{value[5]}
       following => #{value[2]}
       followers => #{value[3]}"
-      puts '=>Tweeters text'
       value[4].each do |t|
         puts "\n =>Tweeters text => #{t}"
       end
--- lib/bot.rb
+++ lib/bot.rb
@@ -18,7 +18,7 @@ class Bot
 
   def searcher(query)
     user = {}
-    @client.search(query).take(3).each do |tweet|
+    @client.search(query).take(1000).each do |tweet|
       if user.key? tweet.user.screen_name
         user[tweet.user.screen_name][5] += 1
         user[tweet.user.screen_name][4] << tweet.full_text
